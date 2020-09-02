36d057de33fc3963cd58d2fe845c7ff79c005a57
Likhithajasthi <likhithajasthi1997@gmail.com>
2020-09-02 15:10:25 -0600

    commit changes

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-var total=0;
+
+/*
+var total;
+total=0;
 document.getElementById('name').textContent='0';
 document.getElementById('totalscore').textContent='0';
-function myName(){
-    var dice= Math.floor(Math.random()*6)+1;
-    console.log("the score is"+dice);
-    /* document.querySelector('#name').textContent=dice;*/
-    document.getElementById('name').innerHTML=dice;
+document.getElementById('player2').textContent='0';
+document.getElementById('score2').textContent='0';
+
+function myName() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    console.log("the score is" + dice);
+    /!* document.querySelector('#name').textContent=dice;*!/
+    document.getElementById('name').innerHTML = dice;
+    /!*diceDOM.style.display='block';*!/
     //adding the score
-    if(dice!==1){
+    if (dice !== 1) {
         //add elements
-        total+=dice;
+        total += dice;
+        document.querySelector('tscore').textContent = total;
+    } else {
+
+    }
+
+
+}*/
+var total;
+    total=0;
+document.getElementById('name').textContent='0';
+document.getElementById('totalscore').textContent='0';
+function myName() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    console.log("the score is" + dice);
+    document.getElementById('name').innerHTML = dice;
+    if (dice !== 1) {
+        //add elements
+        total += dice;
         document.getElementById('totalscore').textContent=total;
+    } else {
+       console.log("invalid")
     }
-    else{
-        console.log("Invalid");
-        nextPlayer();
-    }
+
 }
